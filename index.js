cd1202a43205c85b69ac1079430fa8eb4d1a5c49
arashhaji <56742302+arashhaji@users.noreply.github.com>
2020-02-13 17:57:36 -0500

    mvp stretch

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // code away!
+require("dotenv").config();
+
 const server = require('./server');
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () =>
     console.log(`\n **API running on port ${port}  **\n`)
